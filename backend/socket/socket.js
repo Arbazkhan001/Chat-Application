d5e96ca1276614f8c30891370ee94646b2827275
Arbaz <arbaz@affyclouditsolutions.com>
2024-03-17 11:06:31 +0530

    updates

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,10 +4,11 @@ import express from "express";
 
 const app = express();
 
+
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["http://localhost:3000"], // Allow any origin during development
 		methods: ["GET", "POST"],
 	},
 });
